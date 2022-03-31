7580a50ad2f59aa80bf55dc18587fb08051672d0
Indygos <cyberindygos@gmail.com>
2022-03-31 16:31:32 +0200

    Two

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -6,6 +7,18 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+            var numbers = new [] {34.1, 25.5, 14.7, 69.3};
+            var grades = new List<double>() {34.1, 25.5, 14.7, 69.3};
+            grades.Add(47.2);
+            // var averageScore = grades / 5;
+
+            var result = 0.0;
+            foreach(var number in grades)
+            {
+                result += number;
+            }
+            Console.WriteLine(result);
+
             if (args.Length > 0) 
             {
                 Console.WriteLine("Hello " + args[0] + " !");
